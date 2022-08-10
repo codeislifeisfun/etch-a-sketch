e488613c7a231bb238c57ecdbb229115a6b17972
sourdough <jackie.chan.shopee101@gmail.com>
2022-08-10 14:15:08 +0800

    Flexible number of squares, retain 960px width

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-"use strict"
+//"use strict"
 
-const grid = document.querySelector(".grid");
-const boxesPerRow = 16;
-const pxPerBox = 20;
+function buildGrid(boxesPerRow) {
+    const grid = document.querySelector(".grid");
+    const pxPerBox = 960/boxesPerRow;
+    grid.style.gridTemplateColumns = `repeat(${boxesPerRow},1fr)`;
+    grid.style.width = "960px";
+    for (let i=0; i<boxesPerRow**2; i++){
+        const div = document.createElement("div");
+        div.classList.add('box');
+        grid.appendChild(div);
+        div.style.width = `${pxPerBox}px`;
+        div.style.height = `${pxPerBox}px`;
+    }
 
-grid.style.gridTemplateColumns = `repeat(${boxesPerRow},1fr)`;
-grid.style.width = `${boxesPerRow*(2+pxPerBox)}px`;
-for (let i=0; i<boxesPerRow**2; i++){
-    const div = document.createElement("div");
-    div.classList.add('box');
-    grid.appendChild(div);
-    div.style.width = `${pxPerBox}px`;
-    div.style.height = `${pxPerBox}px`;
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => box.addEventListener('mouseover', () => {
+        box.style.backgroundColor = "black";
+    }));
+
+    const gridSize = document.querySelector('gridSize');
+    gridSize.textContent = `Current dimension is ${boxesPerRow} X ${boxesPerRow}`;
 }
 
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => box.addEventListener('mouseover', () => box.style.background='black'));
+function resizeGrid(){
+    const grid = document.querySelector(".grid");
+    const size = prompt('How many squares per side would you like (5 to 100)?');
+    if (!size || size<5 || size>100) return;
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+
+    buildGrid(size);
+}
+
+buildGrid(64);
+const gridSizeBtn = document.querySelector('button');
+gridSizeBtn.addEventListener('click', resizeGrid);
+
 
